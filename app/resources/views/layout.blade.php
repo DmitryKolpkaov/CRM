b56946b14b8c0083c287c611729f1c94b9b90d5f
dmitriy <dimas1gold@gmail.com>
2024-02-02 15:49:23 +0600

    Добавил tailwindcss, начал делать дизайн прежде чем вносить правки в логику

--- app/resources/views/layout.blade.php
+++ app/resources/views/layout.blade.php
@@ -11,6 +11,7 @@
 
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
